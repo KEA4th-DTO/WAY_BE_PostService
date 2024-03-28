2006d3b462e01298bc7c615d8ccc1eb7ae6493bd
yseo14 <lmslys14@naver.com>
2024-03-28 23:16:58 +0900

    DTO-115 Feat: ApiResponse, Exception handler 추가

---
+++ src/main/java/com/dto/way/post/global/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package com.dto.way.post.global.exception;
+
+import com.dto.way.post.global.response.code.BaseErrorCode;
+import com.dto.way.post.global.response.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus() {
+        return this.code.getReasonHttpStatus();
+    }
+}
