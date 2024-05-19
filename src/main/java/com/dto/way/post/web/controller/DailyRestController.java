763ece9367aebd1e7dd2e71178ffc40fa6a97986
yseo14 <lmslys14@naver.com>
2024-05-20 00:37:56 +0900

    Fix: CORS 필요 없는 설정 제거

--- src/main/java/com/dto/way/post/web/controller/DailyRestController.java
+++ src/main/java/com/dto/way/post/web/controller/DailyRestController.java
@@ -17,7 +17,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/post-service/daily")
 @RequiredArgsConstructor
 public class DailyRestController {
