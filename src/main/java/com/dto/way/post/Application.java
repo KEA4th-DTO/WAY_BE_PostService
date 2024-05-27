b13bc293cb6a442de9c7b1bd98b74a2e93f04dd3
yseo14 <lmslys14@naver.com>
2024-05-28 08:54:33 +0900

    Feat: @EnableAsync 어노테이션 추가

--- src/main/java/com/dto/way/post/Application.java
+++ src/main/java/com/dto/way/post/Application.java
@@ -5,13 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.TimeZone;
 
 
+@EnableAsync
+@EnableFeignClients
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication()
-@EnableFeignClients
 public class Application {
 
 	public static void main(String[] args) {
