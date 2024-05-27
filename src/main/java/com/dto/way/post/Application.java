a100b6f6af62ca935cabc26fb7d22c8c53396805
yseo14 <lmslys14@naver.com>
2024-05-27 14:02:09 +0900

    Chore: timeZone을 한국으로 변경

--- src/main/java/com/dto/way/post/Application.java
+++ src/main/java/com/dto/way/post/Application.java
@@ -1,9 +1,12 @@
 package com.dto.way.post;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 
 @EnableJpaAuditing // JPA Auditing 활성화
@@ -14,6 +17,13 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println(org.hibernate.Version.getVersionString());
+	}
+
+	@PostConstruct
+	public void init() {
+		// 타임존 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("Current TimeZone: " + TimeZone.getDefault().getID());
 	}
 
 }
